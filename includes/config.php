827a24ca440f55fa559153a5f60b34027cf2032c
Maruf-Creative <tegarmaruf1234@gmail.com>
2026-06-22 20:55:42 +0700

    config update 2

--- includes/config.php
+++ includes/config.php
@@ -7,8 +7,9 @@ $db_host = 'reseau.proxy.rlwy.net';
 $db_user = 'root';
 $db_pass = 'xtTNsewYEFcmNNLxrtUSaRhqaSceBuqG';
 $db_name = 'db_kebun_binatang';
+$db_port = 43487;
 
-$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
 
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
